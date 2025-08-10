b0d5fd306f2a3159c23d4fdd858a00e25c783184
emirtotic <emirtotic@gmail.com>
2025-08-11 01:14:40 +0300

    Seed mood entries added to flyway

---
+++ src/main/resources/db/migration/V3__Insert_user_into_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (first_name, last_name, email, password, enabled, role)
+VALUES ('Emir', 'Totic', 'emirtotic@gmail.com', 'password', 1, 'USER');
---
+++ src/main/resources/db/migration/V4__Insert_mood_entries_into_table.sql
@@ -0,0 +1,13 @@
+INSERT INTO mood_entries (user_id, entry_date, mood_score, note) VALUES
+ (1,'2025-08-08',5,'I am happy to say I had an awesome day.'),
+ (1,'2025-08-07',5,'Today I am fine!'),
+ (1,'2025-08-06',4,'Today was a good day but I feel a little tired...'),
+ (1,'2025-08-05',2,'Oh, I would like a couple of days off, I am feeling tired'),
+ (1,'2025-08-04',5,'My mom feels ok today, I am excelent!'),
+ (1,'2025-08-03',1,'I am exausted'),
+ (1,'2025-08-02',3,'I am ok but tired'),
+ (1,'2025-08-01',5,'I meet a girl today, I am kinda in love on first sight'),
+ (1,'2025-07-31',2,'I am ok but I could not sleep last night'),
+ (1,'2025-07-30',4,'Doctor said my injury is not big so I dont feel pain anymore'),
+ (1,'2025-07-29',1,'I had a small injury at the gym'),
+ (1,'2025-07-28',3,'It was ok but I had a new task at work with a soon deadline');
